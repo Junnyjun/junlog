84745a982760ee513331ccfef50892dce629ac61
junnyJun <chbe5082@naver.com>
2022-12-09 14:37:54 +0900

    feat

--- join/src/test/java/git/io/join/IntegrationTestBase.java
+++ join/src/test/java/git/io/join/IntegrationTestBase.java
@@ -1,6 +1,8 @@
 package git.io.join;
 
+import org.junit.jupiter.api.Disabled;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest(args = {""})
 public interface IntegrationTestBase { }
