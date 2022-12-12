3041f4ed76b83f70eda9a748fbfe1fc21c47ca6f
junnyJun <chbe5082@naver.com>
2022-12-12 14:49:02 +0900

    feat(security) : set ACL

--- join/src/test/java/git/io/join/adapter/Fixture.java
+++
@@ -1,7 +0,0 @@
-package git.io.join.adapter;
-
-import git.io.join.application.out.token.TokenOutPort;
-
-public class Fixture {
-    public static final TokenOutPort TOKEN_OUT_PORT = (KEY) -> 0L;
-}
