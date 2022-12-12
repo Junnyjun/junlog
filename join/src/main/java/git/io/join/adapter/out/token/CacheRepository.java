8ccdd6989602f384b1b9901c65599638de1a6433
junnyJun <chbe5082@naver.com>
2022-12-12 17:04:30 +0900

    feat(security) : set ACL

--- join/src/main/java/git/io/join/adapter/out/token/CacheRepository.java
+++ join/src/main/java/git/io/join/adapter/out/token/CacheRepository.java
@@ -4,11 +4,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
 import org.springframework.stereotype.Repository;
+
+import java.util.concurrent.ConcurrentMap;
 
 public interface CacheRepository {
     Boolean isExists(String key);
     void save(String key);
+
+    Integer size();
 
     @Slf4j
     @Repository
@@ -25,6 +30,11 @@ public interface CacheRepository {
         @Cacheable(value = "cache", key = "#key")
         public void save(String key) {
             log.info("Token Saved ::: {}", key);
+        }
+
+        @Override
+        public Integer size() {
+            return ((ConcurrentMapCache) cacheManager.getCache("cache")).getNativeCache().size();
         }
     }
 }
--- join/src/main/java/git/io/join/application/in/token/TokenUseCase.java
+++ join/src/main/java/git/io/join/application/in/token/TokenUseCase.java
@@ -22,13 +22,17 @@ public interface TokenUseCase {
 
         @Override
         public String makeToken(EmailRequest emailRequest) {
+            if (cacheRepository.size() > 5){
+                throw new RuntimeException("인증키 요청이 너무 많습니다. 잠시 후 다시 요청해 주세요.");
+            }
+
             if(cacheRepository.isExists(emailRequest.email)){
                 Long timer = userRepository.timer(emailRequest.email);
                 throw new RuntimeException("이미 인증키가 발급되었습니다." + timer+ "초 후 다시 요청해 주세요.");
             }
-            cacheRepository.save(emailRequest.email);
 
-            String uuid = UUID.randomUUID().toString();
+            cacheRepository.save(emailRequest.email);
+            final String uuid = UUID.randomUUID().toString();
 
             textMailSend.send(request(emailRequest.email, uuid));
             userRepository.init(emailRequest.email, uuid);
